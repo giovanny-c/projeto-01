3066c9e90351766214f3542b7807100308148efa
Giovanny Castanharo <giovanycast@hotmail.com>
2022-04-24 00:00:43 -0300

    creating donors table

---
+++ src/database/migrations/1650766391892-CreateTableDonations.ts
@@ -0,0 +1,83 @@
+import { Generated, MigrationInterface, QueryRunner, Table } from "typeorm"
+
+export class CreateTableDonations1650766391892 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "donations",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "donation_number",
+                        type: "numeric",
+                        //isGenerated
+                    },
+                    {
+                        name: "user_id",
+                        type: "uuid",
+                        isNullable: false
+                    },
+                    {
+                        name: "donor_id",
+                        type: "uuid",
+                        isNullable: false
+
+                    },
+                    {
+                        name: "donation_value",
+                        type: "numeric",
+                        isNullable: false
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                    {
+                        name: "payed_at",
+                        type: "timestamp"
+                    },
+                    {
+                        name: "is_payed",
+                        type: "boolean",
+                        default: false
+                    }
+
+                ],
+
+                foreignKeys: [
+                    {
+                        name: "FKDonorsDonations",
+                        referencedTableName: "donors",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["donor_id"],
+                        onDelete: "SET NULL",
+                        onUpdate: "SET NULL" //Só vai ser null a foreign key?
+
+                    },
+                    {
+                        name: "FKuUserDonations",
+                        referencedTableName: "users",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["user_id"],
+                        onDelete: "SET NULL",
+                        onUpdate: "SET NULL" //Só vai ser null a foreign key?
+
+                    }
+                ]
+            })
+        )
+
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("donations")
+    }
+
+
+}
---
+++ src/modules/donations/entities/donation.ts
@@ -0,0 +1,50 @@
+import { Column, CreateDateColumn, Entity, Generated, JoinColumn, ManyToMany, ManyToOne, PrimaryColumn } from "typeorm";
+import { Donor } from "../../donor/entities/donor";
+
+import { v4 as uuidV4 } from "uuid"
+import { User } from "../../user/entities/user";
+
+@Entity("donations")
+class Donation {
+
+    @PrimaryColumn()
+    id?: string
+
+    @Column()
+    @Generated("increment")
+    donation_number: Number
+
+    @ManyToOne(() => User)
+    @JoinColumn({ name: "user_id" })
+    user: User
+
+    @Column()
+    user_id: string
+
+    @ManyToOne(() => Donor)
+    @JoinColumn({ name: "donor_id" })
+    donor: Donor
+
+
+    @Column()
+    donor_id: string
+
+    @Column()
+    donation_value: Number
+
+    @CreateDateColumn()
+    created_at: Date
+
+    @Column()
+    payed_at: Date
+
+    @Column()
+    is_payed: boolean
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuidV4()
+        }
+    }
+
+}
