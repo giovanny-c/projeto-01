04acf71531f77fab0ee118fd1d566d519ddca98e
Giovanny Castanharo <giovanycast@hotmail.com>
2023-06-06 19:38:05 -0300

    fix datasource

--- src/database/index.ts
+++ src/database/index.ts
@@ -12,7 +12,7 @@ export const dataSource: DataSource = new DataSource({
     //ou em ambiente de produção, 
     //deixar host como localhost
     //@ts-expect-error
-    port: process.env.PRODUCTION? `${(+process.env.DB_MASK_PORT)}:${+(process.env.DB_PORT)}`: +(process.env.DB_PORT),
+    port: process.env.PRODUCTION? `${+(process.env.DB_MASK_PORT)}:${+(process.env.DB_PORT)}`: +(process.env.DB_PORT),
     username: process.env.DB_USER as string,
     password: process.env.DB_PASSWORD as string,
     database: process.env.DB_NAME as string, //nome do banco de dados (environment POSTGRES_DB=)
