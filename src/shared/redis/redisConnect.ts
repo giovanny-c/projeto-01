3d4b396249f33e05067004dfcd64577eea4f4d5c
Giovanny Castanharo <giovanycast@hotmail.com>
2025-06-16 14:56:05 -0300

    Update redisConnect.ts

--- src/shared/redis/redisConnect.ts
+++ src/shared/redis/redisConnect.ts
@@ -2,7 +2,7 @@ import { redisClient } from "./redisConfig"
 
 
 redisClient.on("error", (err) => {
-    console.log(`Could not establish a connection with redis. ${err}`)
+    console.error(`Could not establish a connection with redis.`, err)
 })
 redisClient.on("connect", (err) => {
     console.log("Connected to redis!")
